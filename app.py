f868080b0143fe7f4196c10ed5d28f891bf1fddc
„Andi <ajerni@gmail.com>
2023-07-05 19:22:20 +0200

    googol

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ st.write(result)
 
 
 #tools = load_tools(["serpapi"], llm=llm)
-tools = load_tools(["Intermediate Answer"], llm=llm)
-get_image_agent = initialize_agent(tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH, verbose=True)
+tools = load_tools(["serpapi"], llm=llm)
+get_image_agent = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 bild = get_image_agent.run(f"give me a www link to an image that looks like {result}. Reply with the hyperlink only.")
 
 st.write(bild)
