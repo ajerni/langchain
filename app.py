59be672338353eac4e0262c9866e64deaf086a94
„Andi <ajerni@gmail.com>
2023-07-05 18:14:09 +0200

    googol

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
+from langchain.utilities import SerpAPIWrapper
 
-st.header("""
+import os
+os.environ["SERPAPI_API_KEY"] = st.secrets["serpapi_api_key"]
+
+def get_image(query_text):
+
+    serpapi = SerpAPIWrapper()
+
+    search_result = serpapi.run(query_text, num_images=1)
+
+    image_url = search_result["images"][0]["url"]
+
+    return image_url
+
+st.header(
+    """
 :blue[Andi's Streamlit-Langchain-Food-Bot]
-""")
+"""
+)
 
 my_country = st.text_input("Enter country")
 
-llm = OpenAI(model_name='gpt-3.5-turbo', openai_api_key=st.secrets["openai_api_key"])
+llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets["openai_api_key"])
 
 template = """
 let me know a typical meal from {country}.
@@ -27,3 +43,9 @@ result = llm(final_prompt)
 st.write(":blue[Here comes a typical meal of the country you entered:]")
 
 st.write(result)
+
+resulting_image = get_image(result)
+
+print(resulting_image)
+
+st.image(resulting_image)
