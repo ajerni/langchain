bfe4e863aa95b130d90c5bd220fb700625df3e12
„Andi <ajerni@gmail.com>
2023-07-06 10:56:19 +0200

    googol

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
-from langchain.utilities import SerpAPIWrapper
-from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.utilities import GoogleSerperAPIWrapper
+
+# from langchain.agents import load_tools, initialize_agent, AgentType
 
 import os
-os.environ["SERPAPI_API_KEY"] = st.secrets["serpapi_api_key"]
+import pprint
+
+os.environ["SERPER_API_KEY"] = st.secrets["serper_api_key"]
 
 llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets["openai_api_key"])
 
@@ -15,9 +18,9 @@ st.header(
 """
 )
 
+# st.markdown("![kaze](https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg)")
+
 my_country = st.text_input("Enter country")
-
-
 
 template = """
 let me know a typical meal from {country}.
@@ -37,10 +40,31 @@ st.write(":blue[Here comes a typical meal of the country you entered:]")
 
 st.write(result)
 
+# tools = load_tools(["serpapi"], llm=llm)
+# get_image_agent = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+# bild = get_image_agent.run(f"give me a www link to an image that looks like {result}. Reply with the hyperlink only.")
+#
+# st.write(bild)
 
-#tools = load_tools(["serpapi"], llm=llm)
-tools = load_tools(["serpapi"], llm=llm)
-get_image_agent = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-bild = get_image_agent.run(f"give me a www link to an image that looks like {result}. Reply with the hyperlink only.")
+if result:
+    search = GoogleSerperAPIWrapper(type="images", num=1)
+    image_result = search.results(result)
+    pprint.pp(image_result) #just to display it properly in the terminal
 
-st.write(bild)
+    #who needs a json parser etc. :-) English is my new prefered programming language :-)
+    img_template = """
+    Look through this data {img_json} and find the first hyperlink that ends with .jpg under 'imgUrl' entry. Reply with that hyperlink only. No other text in the reply.
+    """
+
+    img_template_prompt = PromptTemplate(
+        input_variables=["img_json"],
+        template=img_template,
+    )
+
+    img_template_prompt_final = img_template_prompt.format(img_json=image_result)
+
+    link_to_image = llm(img_template_prompt_final)
+
+    st.write(link_to_image)
+
+    st.image(link_to_image, width=250)
